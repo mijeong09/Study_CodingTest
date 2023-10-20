e1be82ba7bde3cb6128adc5d151daae2353ba262
MEMEME <134994706+mijeong09@users.noreply.github.com>
2023-10-21 08:57:57 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int H,M;
+    cin >> H >> M;
+    
+    if(M >= 45)
+    {
+        cout << H << " " << M - 45;
+    }
+    else
+    {
+        if(H == 0)
+        {
+            cout << 23 << " " << 60+M - 45;
+        }
+        else
+        {
+            cout << H - 1 << " " << 60+M - 45;
+        }
+    }
+    
+    return 0;
+}
