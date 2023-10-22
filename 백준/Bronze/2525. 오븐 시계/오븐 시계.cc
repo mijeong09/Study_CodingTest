2adfa246dcaad8b85fc7ce3451a152896f658585
MEMEME <134994706+mijeong09@users.noreply.github.com>
2023-10-22 21:11:13 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -1,19 +1,16 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
-int main() 
+
+int main()
 {
-	ios_base::sync_with_stdio(false);
- 
-	int A, B, C;
-	cin >> A >> B >> C;
- 
-	int min = 60 * A + B;
-	min += C;
- 
-	int hour = (min / 60) % 24;
-	int minute = min % 60;
- 
-	cout << hour << " " << minute;
-	return 0;
+    int h, m, t;
+    cin >> h >> m >> t;
+    
+    int minute = (h * 60 + m) + t;
+    int hour = minute / 60 % 24;
+    minute %= 60;
+    
+    cout << hour << " " << minute;
+    
+    return 0;
 }
