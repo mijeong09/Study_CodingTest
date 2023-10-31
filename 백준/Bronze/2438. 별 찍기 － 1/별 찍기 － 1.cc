ed4e176708b096b9dc6ef88d21596d27dc7fdba9
MEMEME <134994706+mijeong09@users.noreply.github.com>
2023-10-31 09:56:06 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -7,7 +7,7 @@ int main()
     cin >> n;
     for(int i = 0; i < n; i++)
     {
-        for(int j = i + 1; j > 0; j--)
+        for(int j = 0; j <= i; j++)
         {
             cout << "*";
         }
