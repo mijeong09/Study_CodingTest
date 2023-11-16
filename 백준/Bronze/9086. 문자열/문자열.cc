0b279073e29367a52c15a3450f85351127a093b6
MEMEME <134994706+mijeong09@users.noreply.github.com>
2023-11-16 10:34:07 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    string s;
+    
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> s;
+        cout << s[0] << s[s.length() - 1] <<"\n";
+    }
+    return 0;
+}
