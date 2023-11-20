dc369a931dcdb10f91cec811807461f3b209fc14
MEMEME <134994706+mijeong09@users.noreply.github.com>
2023-11-20 16:32:38 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -1,14 +1,15 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
-int main(){
+
+int main()
+{
     int n;
-    string s;
-    cin >> n;
-    cin >> s;
+    string num;
     int sum = 0;
-    for (int i = 0; i < n; i++){
-        sum += s[i] - '0';
+    cin >> n >> num;
+    for(int i = 0; i < n; i++)
+    {
+        sum += num[i] - '0';
     }
     cout << sum;
     return 0;
