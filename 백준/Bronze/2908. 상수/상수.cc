5b3cb61babe0ae3e6d4825493101565d591c5bc7
MEMEME <134994706+mijeong09@users.noreply.github.com>
2023-11-22 18:55:30 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    
+    a = (a / 100) + ((a % 100) / 10) * 10 + (a % 10) * 100;
+    b = (b / 100) + ((b % 100) / 10) * 10 + (b % 10) * 100;
+    if(a > b)
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << b;
+    }
+    return 0;
+}
