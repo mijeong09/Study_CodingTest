f805c58d092d1de7e0b5597c70a1dc3633621d37
MEMEME <134994706+mijeong09@users.noreply.github.com>
2023-12-04 10:02:01 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -1,26 +1,25 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-    int a[101][101], b[101][101];
+    int a[100][100], b[100][100];
     int n, m;
+    
     cin >> n >> m;
     
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
             cin >> a[i][j];
     
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
             cin >> b[i][j];
     
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-            cout << a[i][j] + b[i][j] << ' ';
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cout << a[i][j] + b[i][j] << " ";
         cout << '\n';
-    }
     
     return 0;
 }
