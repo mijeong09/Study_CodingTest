29ab8304385796a5b4cc243ec1843915f170fc31
MEMEME <134994706+mijeong09@users.noreply.github.com>
2023-12-24 15:37:24 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+ 
+    if(a==60&&b==60&&c==60)
+        cout<<"Equilateral";
+ 
+    else if(a+b+c==180)
+    {
+        if(a==b||a==c||b==c)
+            cout<<"Isosceles";
+        
+        else if(a!=b||a!=c||b!=c)
+            cout<<"Scalene";
+    }
+ 
+    else
+        cout<<"Error";
+ 
+    return 0;
+}
