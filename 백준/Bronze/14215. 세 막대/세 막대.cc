e90fce1bb32d61c74ec7a496338076369b0804d0
MEMEME <134994706+mijeong09@users.noreply.github.com>
2023-12-25 12:56:02 +0900

    [Bronze III] Title: 세 막대, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.cc
+++ 백준/Bronze/14215. 세 막대/세 막대.cc
@@ -8,9 +8,9 @@ int main()
     cin >> a[0] >> a[1] >> a[2];
     sort(a, a + 3);
     
-    if(a[0] + a[1] > a[2])
+    if (a[0] + a[1] > a[2])
         cout << a[0] + a[1] + a[2];
-    else 
+    else
         cout << (a[0] + a[1]) * 2 - 1;
     
     return 0;
