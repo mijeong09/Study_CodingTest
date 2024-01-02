9348be1ece1a4f7c695fe4ab9a89a4600854d7ea
MEMEME <134994706+mijeong09@users.noreply.github.com>
2024-01-02 16:13:39 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    
+    cin >> a >> b >> c >> d;
+    
+    if (a * d + b <= c * d && a <= c)
+        cout << 1;
+    else 
+        cout << 0;
+
+    return 0;
+}
