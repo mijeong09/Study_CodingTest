c7e9a93df0ca4799bcc9846cf6f924f28ddc94ac
MEMEME <134994706+mijeong09@users.noreply.github.com>
2024-01-05 14:23:04 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int a, b, c, d, e, f;
+int main() 
+{
+	cin >> a >> b >> c >> d >> e >> f;
+
+	for (int i = -999; i <= 999; i++) 
+    {
+		for (int j = -999; j <= 999; j++) 
+        {
+			if ((i * a + j * b == c) && (i * d + j * e == f)) 
+            {
+				cout << i << " " << j;
+				return 0;
+			}
+		}
+	}
+}
