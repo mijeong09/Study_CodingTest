e656de014663928873c751cec4e770707ac3970a
MEMEME <134994706+mijeong09@users.noreply.github.com>
2024-01-08 18:26:55 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, cnt = 0;
+    cin >> a;
+    
+    while (a>=0)
+    {
+        if(a%5==0)
+        {
+            cnt += a/5;
+            cout << cnt;
+            return 0;
+        }
+
+        a-=3;
+        cnt++;
+    }
+    
+    cout << -1 ;
+    return 0;
+}
+ 
